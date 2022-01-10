bcc9be9e05a1d68ca88e04605f7b227901131ef0
Akhila499 <akhilathota499@gmail.com>
2022-01-10 18:34:32 -0500

    initial installations and setup

--- react/src/App.js
+++ react/src/App.js
@@ -1,9 +1,11 @@
 import "./scss/App.scss";
-
+import ButtonComponent from "./components/button/ButtonComponent";
 function App() {
   return (
     <div className="home__background">
-      <p className="home__text">Please render your Components Here</p>
+      {/* <p className="home__text">Please render your Components Here</p>
+       */}
+      <ButtonComponent />
     </div>
   );
 }
---
+++ react/src/components/button/ButtonComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@avaya/neo-react";
+
+const ButtonComponent = () => {
+  return (
+    <div>
+      <Button label="default" status="default" variant="primary" />
+    </div>
+  );
+};
+
+export default ButtonComponent;
